dde06aca0461a9b00db8b39d76c52115b98a0180
xybxbh <xybxbh@gmail.com>
2019-11-11 21:10:28 -0500

    moving+4

--- BayesianSearch.py
+++ BayesianSearch.py
@@ -147,6 +147,53 @@ class BayesianSearch(object):
                 count += 1
                 self.landscape.target_move()
 
+    def search_moving_target_onestep_base(self, rule):
+        motion_count = 0
+        search_count = 0
+        (cur_x, cur_y) = (0, 0)
+        while True:
+            temp_belief = [[self.landscape.env[i][j].belief[-1] for j in range(self.landscape.dim)] for i in range(self.landscape.dim)]
+            for i in range(self.landscape.dim):
+                for j in range(self.landscape.dim):
+                    if self.landscape.env[i][j].type != self.landscape.tracker_history:
+                        valid_neighbor = []
+                        if i - 1 >= 0:
+                            valid_neighbor.append((i - 1, j))
+                        if i + 1 <= self.landscape.dim - 1:
+                            valid_neighbor.append((i + 1, j))
+                        if j - 1 >= 0:
+                            valid_neighbor.append((i, j - 1))
+                        if j + 1 <= self.landscape.dim - 1:
+                            valid_neighbor.append((i, j + 1))
+                        for k in range(len(valid_neighbor)):
+                            (valid_i, valid_j) = valid_neighbor[k]
+                            self.landscape.env[valid_i][valid_j].belief[-1] += temp_belief[i][j] / len(valid_neighbor) # no append
+            for i in range(self.landscape.dim):
+                for j in range(self.landscape.dim):
+                    if self.landscape.env[i][j].type == self.landscape.tracker:
+                        self.landscape.env[i][j].belief[-1] = 0
+            self.landscape.normalize()
+            if rule == 1:
+                (x, y) = self.landscape.get_cell_with_highest_belief()
+            if rule == 2:
+                (x, y) = self.landscape.get_cell_with_highest_p_of_finding()
+            cell = self.landscape.env[x][y]
+            search_count += 1
+            motion_count += abs(x - cur_x) + abs(y - cur_y)
+            action_count = motion_count + search_count
+            if cell.search_cell():
+                return (x, y), action_count, motion_count, search_count  # ending condition
+            else:
+                for i in range(self.landscape.dim):
+                    for j in range(self.landscape.dim):
+                        if i == x and j == y:
+                            pass  # update (x, y) at last
+                        else:
+                            self.landscape.env[i][j].update_belief(1 / (1 - cell.belief[-1] * (1 - cell.fn)))
+                cell.update_belief(cell.fn / (cell.belief[-1] * cell.fn + 1 - cell.belief[-1]))
+                count += 1
+                self.landscape.target_move()
+
     def search_moving_target_onestep(self, rule):
         motion_count = 0
         search_count = 0
@@ -186,7 +233,6 @@ class BayesianSearch(object):
             action_count = motion_count + search_count
             (cur_x, cur_y) = (x, y)
             if cell.search_cell():
-                count += 1
                 return (x, y), action_count, motion_count, search_count  # ending condition
             else:
                 for i in range(self.landscape.dim):
